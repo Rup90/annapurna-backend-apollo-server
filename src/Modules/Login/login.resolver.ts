25ec18325823dccae3d44b3ebe32bb35b2936455
Rupayan Dey <rupayan.dey1990@gmail.com>
2020-07-28 18:39:39 +0530

    used union for login

--- src/Modules/Login/login.resolver.ts
+++ src/Modules/Login/login.resolver.ts
@@ -2,11 +2,25 @@ export default {
     Query: {
         login: (parent: any, args: any, context: any, info: any) => {
             console.log('args ==>', args);
+
+            if (args.email === 'test@test.com') {
+                return {
+                    __typename: 'LoginSuccessResponse',
+                    statusCode: 200,
+                    response: {
+                        token: 'xxxxxxx',
+                        role: 'FARMER',
+                        user_id: '123456',
+                        expiresIn: 60
+                    }
+                }
+            }          
             return {
-                token: 'xxxxxxx',
-                role: 'FARMER',
-                user_id: '123456',
-                expiresIn: 60
+                __typename: 'LoginFaliureResponse',
+                statusCode: 422,
+                response: {
+                    message: `Invalid credentials`,
+                }
             };
         }
     }
--- src/Modules/Login/login.schema.ts
+++ src/Modules/Login/login.schema.ts
@@ -5,11 +5,27 @@ const typeDefs = gql`
         login(email: String!, password: String!): LoginResponse
     }
 
-    type LoginResponse {
+    union LoginResponse = LoginSuccessResponse | LoginFaliureResponse
+
+    type LoginSuccessResponse {
+        statusCode: Int!
+        response: SuccessResponse!
+    }
+
+    type LoginFaliureResponse {
+        statusCode: Int!
+        response: FailureResponse!
+    }
+
+    type SuccessResponse {
         user_id: String!
         role: String!
         token: String!
         expiresIn: Int!
+    }
+
+    type FailureResponse {
+        message: String!
     }
 `;
 
